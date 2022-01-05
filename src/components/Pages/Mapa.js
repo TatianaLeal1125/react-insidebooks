8d38a5ae542813e403038c0265bec69b0733ec7e
TatianaLeal1125 <mileydy.1125@gmail.com>
2022-01-05 16:25:51 -0600

    Working with Mapa view. Not complete yet

--- src/components/Pages/Mapa.js
+++ src/components/Pages/Mapa.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api'
+import keyMaps from './credentials'
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+}
 
 function Mapa () {
   return (
-      <div>
-          <h3>Estás en Mapa</h3>
-      </div>
+    <LoadScript googleMapsApiKey='AIzaSyCf38zjlzEzWkwk8qxrTW0rmVW_ClTLS34'>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/*Child components, such as markers, info windowss*/}
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
